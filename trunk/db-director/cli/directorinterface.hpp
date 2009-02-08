6166ffa11744c0452ecedff5743ea1a95c4e1486
finalman <finalman@75511648-93a4-11dd-b566-b74dd1c53f51>
2009-02-08 13:32:30 +0000

    First steps in director.

---
+++ trunk/db-director/cli/directorinterface.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <db-util.hpp>
+
+using namespace System;
+
+#ifndef DB_USE_DIRECTOR
+#error You should define DB_USE_DIRECTOR to 0 or 1
+#endif
+
+#if DB_USE_DIRECTOR
+#define getTweakFloat(name)  (director.getTweakableByName(#name, Tweakable::TypeInt).getFloat())
+#define getTweakInt(name)    (director.getTweakableByName(#name, Tweakable::TypeFloat).getInt())
+#define getTweakVector(name) (director.getTweakableByName(#name, Tweakable::TypeVector).getVector())
+#define getTweakSpline(name) (director.getTweakableByName(#name, Tweakable::TypeSpline).getSpline())
+#else
+#include "tweakconstants.hpp"
+#define getTweakFloat(name)  (name)
+#define getTweakInt(name)    (name)
+#define getTweakVector(name) (name)
+#define getTweakSpline(name) (name)
+#endif
+
+namespace Verkstan
+{
+	public ref class Tweakable
+	{
+	public:
+		enum Type
+		{
+			TypeInt,
+			TypeFloat,
+			TypeVector,
+			TypeSpline
+		};
+
+		Tweakable(string _name, Type type) :
+		  name(_name),
+		  type(_type),
+		  floatVal(0.0f),
+		  intVal(0),
+		  vecVal(0.0f, 0.0f, 0.0f),
+		  splineVal()
+		{}
+
+		float getFloat();
+		void setFloat(float v);
+
+		int getInt();
+		void setInt(int v);
+
+		Vec3 getVector();
+		void setVector(Vec3 v);
+
+		Spline getSpine();
+		void setSpine(Spline v);
+
+		string getName();
+		
+	private:
+		Type type;
+		string name;
+		Vec3 vecVal;
+		float floatVal;
+		int intVal;
+		Spline splineVal;
+	};
+
+    public interface IDirectorInterface
+    {
+    public:
+        int getTick();
+		int getLength();
+		void setTick(int tick);
+		void run();
+		void stop();
+		
+		int getNumTweakables();
+		Tweakable getTweakableByIndex(int index);
+		Tweakable getTweakableByName(string name, Type type);
+    };
+}
---
+++ trunk/db-director/editor/Transport.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Design;
+//using VerkstanEditor.Logic;
+
+namespace VerkstanEditor.Gui
+{
+    [ToolboxItem(true)]
+    public partial class Transport : UserControl
+    {/*
+        #region Properties
+        private Project project;
+        public Project Project
+        {
+            get
+            {
+                return project;
+            }
+            set
+            {
+                project = value;
+                if (project != null)
+                {
+                    bpm.Value = project.BPM;
+                    Metronome.BPM = project.BPM;
+                }
+            }
+        }
+        #endregion
+        */
+        #region Private Variables
+        private bool showClockAsBeats = true;
+        private int tick;
+        #endregion
+
+        #region Constructor
+        public Transport()
+        {
+            InitializeComponent();            
+            UpdateClock();
+            //Metronome.BeatChangedFastUpdate += new Metronome.BeatChangedHandler(this.Transport_BeatChangedFastUpdate);
+        }
+        #endregion
+
+        #region Event Handlers
+        public void Transport_BeatChangedFastUpdate(int tick)
+        {
+            this.tick = tick;
+            UpdateClock();
+        }
+        private void play_Click(object sender, EventArgs e)
+        {
+            //Metronome.Loop = false;
+            //Metronome.Start();
+        }
+        private void stop_Click(object sender, EventArgs e)
+        {
+            //Metronome.Pause();
+            //Metronome.Tick = 0;
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            //Metronome.Tick = 0;
+        }
+        private void pause_Click(object sender, EventArgs e)
+        {
+            //Metronome.Pause();
+        }
+        private void bpm_ValueChanged(object sender, EventArgs e)
+        {
+            //Metronome.BPM = Convert.ToInt32(this.bpm.Value);
+            //if (project != null)
+            //    project.BPM = Metronome.BPM;
+        }
+        private void beatsOrTime_SelectedValueChanged(object sender, EventArgs e)
+        {
+            UpdateClock();
+
+            int clockX = Size.Width / 2 - this.time.Size.Width / 2;
+            this.time.Location = new Point(clockX, this.time.Location.Y);  
+        }
+        private void Transport_Resize(object sender, EventArgs e)
+        {
+            int clockX = Size.Width / 2 - this.time.Size.Width / 2;
+            this.time.Location = new Point(clockX, this.time.Location.Y);
+
+            this.miniTimeline1.Size = new Size(Size.Width - 8, miniTimeline1.Size.Height);
+        }
+        private void loopPlay_Click(object sender, EventArgs e)
+        {
+           //Metronome.Loop = true;
+           // Metronome.Start();
+        }
+        #endregion
+
+        #region Private Methods
+        private void UpdateClock()
+        {
+          
+            if (showClockAsBeats)
+            {
+                //float bf = this.tick / (float)Metronome.TicksPerBeat;
+                //this.time.Text = String.Format("{0:0000.00}", bf + 1);
+            }
+            else
+            {
+                //TimeSpan ts = TimeSpan.FromMilliseconds(Metronome.Milliseconds);
+                //this.time.Text = String.Format("{0:00}:{1:00}:{2:00}", (int)ts.TotalMinutes, ts.Seconds, (int)(ts.Milliseconds / 10.0f));
+            }
+        }
+        #endregion
+    }
+}
